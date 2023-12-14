a0dbd9fec9b4bd377d7487c06692ed8af5c18494
sprinzs123 <sprinzs351@gmail.com>
2023-12-13 22:06:49 -0500

    need to add polymorphism and classes option

--- groupProject.cpp
+++ groupProject.cpp
@@ -23,12 +23,13 @@ void reset() { printf("\033[1;0m"); }
 
 
 // global varibles, mostly to know size of arrays
-int foodIndex = 0;
-int cardCount = 0;
-char menuReturn;
-char userValidator;
-int menuOption;
-string checkID = "64337";
+int foodIndex = 0; // size of food indexx
+int cardCount = 0; // size of card index
+int giftCount = 0; // size of gift card index
+char menuReturn; // menu location
+char userValidator; // menu validation
+int menuOption; // option menu
+string checkID = "64337"; // check id
 
 // #### New Classes ####
 class CheckMetaData {
@@ -114,6 +115,7 @@ public:
 	string printSource() { return "Tran DataSource: " + source; }
 	string printAmout() { return to_string(amount); }
 	float getAmount() { return amount; }
+	string getLablel() { return label; }
 	string printHeader() { return printTransaction() + "\n" + "\n" +  printAID() + "\n" + printTC() + "\n" + printLabel() + "\n" + printVerification() + "\n" + printSource() + "\n"; }
 	//string printHeader() { return printTransaction(); }
 
@@ -145,10 +147,34 @@ class FoodItem {
 };
 
 
+class GiftCard {
+private:
+	float amount;
+	int cardId;
+public:
+	GiftCard() {
+		cardId = -1;
+	}
+
+	GiftCard(float _amount, int _cardId) {
+		amount = _amount;
+		cardId = _cardId;
+	}
+
+	int getId() {
+		return cardId;
+	}
+	float getAmount() {
+		return amount;
+	}
+};
+
+
 // ### New Functions ###
 string* substringArr(string, char, int);
 FoodItem selectFoodLine(string, string, int);
 CardPayment selectCardLine(string, string, int);
+GiftCard selectGiftLine(string, int);
 CheckMetaData createMeta();
 void printPrice(float);
 void printHeader(CheckMetaData);
@@ -156,9 +182,9 @@ void printHeader(CheckMetaData);
 
 // ### menu function
 void mainMenu();
-void printOne(FoodItem*, float, CheckMetaData);
+void printOne(FoodItem*, float, CheckMetaData, GiftCard*, CardPayment, float);
 void printTwo(CardPayment, float, CheckMetaData);
-void saveOne(FoodItem*, float, CheckMetaData);
+void saveOne(FoodItem*, float, CheckMetaData, GiftCard*, CardPayment, float);
 void saveTwo(CardPayment, float, CheckMetaData);
 void userInput();
 void printData();
@@ -204,55 +230,85 @@ string* substringArr(string fullString, char delimeter, int delimeterCount) {
 
 // pases lines from food file correctly at matches to needed id
 // use look up table to find food name and price from look up table
-FoodItem selectFoodLine(string chechId, string line, int delimeterCount) {	
-	string foodName;
-	float foodPrice = -1;
-	int foodCount = -1;
+FoodItem selectFoodLine(string chechId, string line, int delimeterCount) {
+	string foodName;  // food name
+	float foodPrice = -1; //defauld food price
+	int foodCount = -1;  // deffault food count
 	if (line.length() > 7 && line.substr(0, 5) == chechId) {
-		string id = line.substr(0, 5);
-		string* arrItems = substringArr(line, '-', delimeterCount);
-		if (arrItems[0] == "-1") {
+		string id = line.substr(0, 5); // get id check
+		string* arrItems = substringArr(line, '-', delimeterCount); // get array of strings
+		if (arrItems[0] == "-1") { // check if parsed correctly
 			//cout << "invalid object" << endl;		
 		}
 		else {
-			//cout << "valid object " << endl;
-			foodCount = stoi(arrItems[2]);
-			string checkFoodId = arrItems[0]; // check id from input line
-			string foodOrderCode = arrItems[1];
-			//cout << checkFoodId << endl;
-			if (chechId == checkFoodId){     
-				//cout << "Getting from look up table" << endl;
-				ifstream foodTable;
-				string line;
-				foodTable.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\food_table.txt", ios::in);
-				if (foodTable.fail()) {
-					//cout << "Wrong file can't open food look up table \n";
-				}
-				else {
-					bool searching = true;
-					while (getline(foodTable, line) && searching) {
-						string* foodItem = substringArr(line, '-', 2);
-						string foodCode = foodItem[0];
-						// checking if food code from look up table matches food code for the item
-						if (foodCode == foodOrderCode) {
-							foodPrice = stof(foodItem[1]);
-							foodName = foodItem[2];
-							searching = false;
-							foodTable.close();
+			try {
+				//cout << "valid object " << endl;
+				foodCount = stoi(arrItems[2]); //str to int
+				string checkFoodId = arrItems[0]; // check id from input line
+				string foodOrderCode = arrItems[1]; // food code
+				//cout << checkFoodId << endl;
+				if (chechId == checkFoodId) {
+					//cout << "Getting from look up table" << endl;
+					ifstream foodTable;
+					string line;
+					foodTable.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\food_table.txt", ios::in);
+					if (foodTable.fail()) {
+						//cout << "Wrong file can't open food look up table \n";
+					}
+					else {
+						// searchihing in look up table
+						bool searching = true;
+						while (getline(foodTable, line) && searching) {
+							string* foodItem = substringArr(line, '-', 2); // string array with for searching
+							string foodCode = foodItem[0]; // get food code from array
+							// checking if food code from look up table matches food code for the item
+							if (foodCode == foodOrderCode) { // found a math
+								foodPrice = stof(foodItem[1]); // assign food price for item
+								foodName = foodItem[2]; // assign food name to item
+								searching = false;
+								foodTable.close();
+							}
 						}
 					}
 				}
 			}
-		}		
-	}	
-	try {
-		FoodItem foodItem = FoodItem(foodName, foodPrice, foodCount);
-		return foodItem;
+
+
+			catch (int e) {
+				FoodItem foodItem = FoodItem("", 0, -1); // default object
+				return foodItem; // couldn't parse files
+			}
+
+		}
+
+		try {
+			FoodItem foodItem = FoodItem(foodName, foodPrice, foodCount); // correct object
+			return foodItem;	// return correct object
+		}
+		catch (int e) {
+			FoodItem foodItem = FoodItem("", 0, -1); // coundnt assign values to object
+			return foodItem; // return invalid object
+		}
 	}
-	catch (int e) {
-		FoodItem foodItem = FoodItem("", 0, -1);
-		return foodItem;
+}
+
+GiftCard selectGiftLine(string line, int delimiterCount) {
+	string* arrItems = substringArr(line, '-', delimiterCount);
+	// valid card, found all items, arraay of string is correct size
+	if (arrItems[0] != "-1" && arrItems[0] == checkID) {
+		try {
+			GiftCard newCard = GiftCard(stoi(arrItems[2]), stof(arrItems[1]));
+			return newCard; // return valid card
+
+		}
+		catch (int e) {
+			return GiftCard(-1, -1); // return invalid card 
+		}
 	}
+	else {
+		return GiftCard(-1, -1); // return invalid card 
+	}
+
 }
 
 // creates credit card object from string
@@ -267,12 +323,12 @@ CardPayment selectCardLine(string chechId, string line, int delimeterCount) {
 			return cardPayment;
 		}
 		catch (int e) {
-			cout << "can't process card info";
+			cout << "can't process card info"; // if can't parse credit card will exit 
 			exit(1);
 		}
 	}
 	else {
-		cout << "can't process card info";
+		cout << "can't process card info"; // if can't parse credit card will exit
 		exit(1);
 	}
 
@@ -284,17 +340,17 @@ CheckMetaData createMeta() {
 	getline(myfile, line);
 	myfile.close();
 
-	string* arrData = substringArr(line, '-', 6);
-	if (arrData[6] == checkID && arrData[0] != "-1") {	
+	string* arrData = substringArr(line, '-', 6); // array of str to create object
+	if (arrData[6] == checkID && arrData[0] != "-1") {	// validate that array is correct
 		try {
-			return CheckMetaData(arrData[0], arrData[1], arrData[2], arrData[3], stoi(arrData[4]), stoi(arrData[5]));
+			return CheckMetaData(arrData[0], arrData[1], arrData[2], arrData[3], stoi(arrData[4]), stoi(arrData[5])); // correct metadata
 		} 
 		catch (string line) {
-			return CheckMetaData("", "", "", "", 0, 0);
+			return CheckMetaData("", "", "", "", 0, 0); // couldn't assign values to object
 		}
 	}
 	else {
-		return CheckMetaData("", "", "", "", 0, 0);
+		return CheckMetaData("", "", "", "", 0, 0); // array was incoretly parsed
 	}
 	
 }
@@ -306,20 +362,20 @@ int main()
 
 	// GETTING ALL VALID FOOD ITEMS
 	// will have an array of all food items
-	FoodItem foodArray[20];
-	ifstream foodFile;
+	FoodItem foodArray[20];  // storing food items
+	ifstream foodFile; 
 	string line;	
-	float price = 0;
+	float price = 0; // proce for all food
 	foodFile.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\food.txt", ios::in);
 	if (foodFile.fail()) {
 		cout << "Wrong file can't open food \n";
 	} else {		
 		while (getline(foodFile, line)) {			
-			FoodItem foodItem = selectFoodLine("64337", line, 2);
+			FoodItem foodItem = selectFoodLine("64337", line, 2); // trying to creaate food item
 			if (foodItem.getCount() != -1) { //get only valid food items
-				foodArray[foodIndex] = foodItem;
-				price += foodItem.getPrice() * foodItem.getCount();
-				foodIndex += 1;
+				foodArray[foodIndex] = foodItem; // addint to array of food 
+				price += foodItem.getPrice() * foodItem.getCount(); // increase total price
+				foodIndex += 1;  // change index
 			}	
 		}
 	foodFile.close();
@@ -327,19 +383,46 @@ int main()
 	
 	// GET VALID CARD PAYMENTS
 	ifstream cardItems;
-	CardPayment cardPayment;
+	CardPayment cardPayment; // credit card info
 	cardItems.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\cards.txt", ios::in);
 	if (cardItems.fail()) {
 		cout << "Wrong file can't open card payments \n";
 	}
 	else {
 		while (getline(cardItems, line)) {
-			CardPayment cardPaymentTemp = selectCardLine("", line, 7);
-			cardPayment = cardPaymentTemp;
+			CardPayment cardPaymentTemp = selectCardLine("", line, 7);  // trying to get new credit card
+			cardPayment = cardPaymentTemp; // new credit card
 
 		}
 		cardItems.close();
 	}
+
+	float card_price = price; // how much to pay for credit card
+	// GET GIFT CARDS
+	GiftCard giftArr[20];  // storing gift cards
+	ifstream giftFile;
+	giftFile.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\giftcard.txt", ios::in);
+	if (giftFile.fail()) {
+		cout << "Wrong file can't open gift cards \n";
+	}
+	else {
+		while (getline(giftFile, line)) {
+			GiftCard giftItem = selectGiftLine(line, 2);
+			if (giftItem.getId() != -1) { //get only valid gift card items
+				giftArr[giftCount] = giftItem; // push to array of git cards
+				card_price -= giftItem.getAmount(); //subtrack what neeed to be payed by card
+				giftCount += 1; // pointer for gift card caount 
+			}
+		}
+	}
+	giftFile.close();
+	
+
+	// checking if need to pay with credit card if gift card covers it
+	if (card_price < 0) {
+		card_price = 0;
+	}
+
 
 	// #### menu items ##############
 	CheckMetaData metaData = createMeta();
@@ -357,7 +440,7 @@ int main()
 		else if (menuOption == 2) {
 			cout << system("cls");
 			cout << endl;
-			printOne(foodArray, price, metaData);
+			printOne(foodArray, price, metaData, giftArr, cardPayment, card_price);
 			userInput();
 		}
 		else if (menuOption == 3) {
@@ -369,7 +452,7 @@ int main()
 		else if (menuOption == 4) {
 			cout << system("cls");
 			cout << endl;
-			saveOne(foodArray, price, metaData);
+			saveOne(foodArray, price, metaData, giftArr, cardPayment, card_price);
 			userInput();
 		}
 		else if (menuOption == 5) {
@@ -433,23 +516,32 @@ void mainMenu(){
 };
 // print first check
 
-void printOne(FoodItem* foodItems, float price, CheckMetaData metadata){
-	
+void printOne(FoodItem* foodItems, float price, CheckMetaData metadata, GiftCard* giftArr, CardPayment cardPayment, float cardPrice){
+	white();
 	printHeader(metadata);
 	cout << endl;
 	// printing food items
+	blue();
 	for (int i = 0; i < foodIndex; i++) {		
 		cout <<  left << setfill(' ') << setw(40) << foodItems[i].getLeft() << right << setfill(' ') << setw(26) << foodItems[i].getPrice() << endl;
 	}
 	cout << endl;
 	printPrice(price);
 	cout << endl;
+	cyan();
+	for (int i = 0; i < giftCount; i++) {
+		cout << left << setfill(' ') << setw(40) << "Gift Card (" + to_string(giftArr[i].getId()) + ")" << right << setfill(' ') << setw(26) << giftArr[i].getAmount() << endl;
+	}
+	cout << left << setfill(' ') << setw(40) << cardPayment.getLablel() << right << setfill(' ') << setw(26) << cardPrice << endl;
+
+	cout << endl;
+	purple();
 	printFooter(metadata);
 
 };
 
 //save first check
-void saveOne(FoodItem* foodItems, float price, CheckMetaData metaData){
+void saveOne(FoodItem* foodItems, float price, CheckMetaData metaData, GiftCard* giftArr, CardPayment cardPayment, float cardPrice){
 	ofstream myfile;
 	myfile.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\output1.txt", ios::in);
 	// header
@@ -477,6 +569,10 @@ void saveOne(FoodItem* foodItems, float price, CheckMetaData metaData){
 	myfile << left << setfill(' ') << setw(40) << "Suggested gratuity is 20% -- $" << right << setfill(' ') << setw(26) << price * 0.20 << endl;
 	myfile << left << setfill(' ') << setw(40) << "Suggested gratuity is 18% -- $" << right << setfill(' ') << setw(26) << price * 0.18 << endl;
 	myfile << endl;
+	for (int i = 0; i < giftCount; i++) {
+		myfile << left << setfill(' ') << setw(40) << "Gift Card (" + to_string(giftArr[i].getId()) + ")" << right << setfill(' ') << setw(26) << giftArr[i].getAmount() << endl;
+	}
+	myfile << left << setfill(' ') << setw(40) << cardPayment.getLablel() << right << setfill(' ') << setw(26) << cardPrice << endl;
 	// footer section
 	myfile << "------------------------------------------------------------------ \n";
 	myfile << "THANKS FOR VISITING US TODAY!\n";
@@ -500,6 +596,7 @@ void saveOne(FoodItem* foodItems, float price, CheckMetaData metaData){
 
 // print second check
 void printTwo(CardPayment cardPayment, float price, CheckMetaData metaData){
+	red();
 	cout << right << setfill(' ') << setw(40) << "321 Main St, City   " << endl;
 	cout << right << setfill(' ') << setw(40) << "609 420 123       " << endl;
 	cout << right << setfill(' ') << setw(40) << "* * * * Dine In * * * *" << endl;
@@ -509,16 +606,18 @@ void printTwo(CardPayment cardPayment, float price, CheckMetaData metaData){
 	cout << metaData.getServer() << endl;
 	cout << left << setfill(' ') << setw(40) << metaData.getDate() + " " + metaData.getTime() << right << setfill(' ') << setw(26) << metaData.guestLine() << endl;
 
-	
+	blue();
 	cout << cardPayment.printHeader();
 	cout << left << setfill('-') << setw(66) << ' ' << endl;
 	cout << left << setfill(' ') << setw(40) << "Card Number" << right << setfill(' ') << setw(26) << "Auth Code" << endl;
 	cout << left << setfill(' ') << setw(40) << cardPayment.printCardNumber() << right << setfill(' ') << setw(26) <<  cardPayment.printChipAuth() << endl << endl;
-	cout << left << setfill(' ') << setw(40) << "Check Amount" << right << setfill(' ') << setw(26) << cardPayment.printAmout() << endl;
+	cout << left << setfill(' ') << setw(40) << "Check Amount" << right << setfill(' ') << setw(26) << price << endl;
 	cout << "------------------------------------------------------------------\n";
 	cout << "Gratuity Not Included. Suggested amounts are\n";
 	cout << "provided for your convenience.\n";
 	cout << "------------------------------------------------------------------\n";
+
+	yellow();
 	cout << left << setfill(' ') << setw(40) << "Suggested gratuity is 22% -- $" << right << setfill(' ') << setw(26) << price * 0.22 << endl;
 	cout << left << setfill(' ') << setw(40) << "Suggested gratuity is 20% -- $" << right << setfill(' ') << setw(26) << price * 0.20 << endl;
 	cout << left << setfill(' ') << setw(40) << "Suggested gratuity is 18% -- $" << right << setfill(' ') << setw(26) << price * 0.18 << endl;
@@ -527,6 +626,8 @@ void printTwo(CardPayment cardPayment, float price, CheckMetaData metaData){
 	cout << left << setfill(' ') << setw(40) << "Total...   " << right << setfill(' ') << setw(26) << "___________";
 	cout << endl << endl;
 	cout << "X";
+
+	white();
 	cout << left << setfill('_') << setw(65) << "" << endl;
 	cout << left << setw(65) << "Cardmember agrees to pay total in accordance \nwith agreement governing use of such card.\n";
 	cout << right << setfill(' ') << setw(40) << "Guest Copy" << endl;
@@ -554,7 +655,7 @@ void saveTwo(CardPayment cardPayment, float price, CheckMetaData metaData){
 	myfile << left << setfill('-') << setw(66) << ' ' << endl;
 	myfile << left << setfill(' ') << setw(40) << "Card Number" << right << setfill(' ') << setw(26) << "Auth Code" << endl;
 	myfile << left << setfill(' ') << setw(40) << cardPayment.printCardNumber() << right << setfill(' ') << setw(26) << cardPayment.printChipAuth() << endl << endl;
-	myfile << left << setfill(' ') << setw(40) << "Check Amount" << right << setfill(' ') << setw(26) << cardPayment.printAmout() << endl;
+	myfile << left << setfill(' ') << setw(40) << "Check Amount" << right << setfill(' ') << setw(26) << price << endl;
 	myfile << "------------------------------------------------------------------\n";
 	printPrice(price);
 
@@ -650,6 +751,7 @@ void userInput() {
 void printData() {
 	ifstream file;
 	string line;
+	white();
 	file.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\food.txt", ios::in); \
 	cout << "Food files \n";
 	while (!file.fail() && !file.eof()) {
@@ -658,7 +760,7 @@ void printData() {
 	}	
 	file.close();
 	cout << endl;
-
+	cyan();
 	file.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\food_table.txt", ios::in); \
 		cout << "Food look up table \n";
 	while (!file.fail() && !file.eof()) {
@@ -668,6 +770,7 @@ void printData() {
 	file.close();
 	cout << endl;
 
+	blue();
 	file.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\visa_payment.txt", ios::in); \
 		cout << "Credit card payment \n";
 	while (!file.fail() && !file.eof()) {
@@ -676,15 +779,17 @@ void printData() {
 
 	}
 	file.close();
-	cout << endl;
-
-	file.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\payments.txt", ios::in); 
+	cout << endl << endl;
+	red();
+	file.open("C:\\Users\\cooke\\Desktop\\school\\c_plus_plus\\groupProject\\groupProject\\giftcard.txt", ios::in); 
 	cout << "Gift Cards \n";
 	while (!file.eof()) {
 		getline(file, line);
-		cout << line;
+		cout << line << endl;
 	}
 	file.close();
+
+
 
 }
 
